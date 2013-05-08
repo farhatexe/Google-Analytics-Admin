c6407fd3436653bd60c0c67412290bd1491822ad
Hasan E Farhat <farhat.exe@gmail.com>
2013-05-08 11:39:11 +0300

    All Version
    All Version

---
+++ Analytics-Project/AnalyticsProject/Models/AnalyticsSummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnalyticsProject.Models
+{
+    public class AnalyticsSummary
+    {
+        public TimeSpan AveragePageLoadTime { get; set; }
+        public IDictionary<string, int> TopSearches { get; set; }
+        public IDictionary<string, int> TopReferrers { get; set; }
+        public IDictionary<string, int> PageViews { get; set; }
+        public IDictionary<string, string> PageTitles { get; set; }
+        public double PercentExitRate { get; set; }
+        public double PageviewsPerVisit { get; set; }
+        public double EntranceBounceRate { get; set; }
+        public TimeSpan AverageTimeOnSite { get; set; }
+        public double PercentNewVisits { get; set; }
+        public int TotalPageViews { get; set; }
+        public int TotalVisits { get; set; }
+        public IList<int> Visits { get; set; }
+    }
+}
